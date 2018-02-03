5b08ff78115409efe29a7c476bb3705e4cb660cd
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2018-02-03 10:03:12 +0530

    Get front's header component and make it work.

---
+++ components/icons/plus.js
@@ -0,0 +1,20 @@
+const Plus = ({ size = 14 }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="174 112 11 12"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g
+      opacity=".25"
+      fill="none"
+      fillRule="evenodd"
+      strokeLinecap="square"
+      stroke="#000"
+    >
+      <path d="M179.5 113v10M174.5 118h10" />
+    </g>
+  </svg>
+)
+
+export default Plus
