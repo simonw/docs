f1554709b87053f47bcf54ee3a48fff2f25c46b6
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2018-02-12 13:32:38 +0530

    Wait until service worker activated before initPrefetching.

--- client/init-prod.js
+++ client/init-prod.js
@@ -4,9 +4,25 @@ const logger = console
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/service-worker.js')
-    .then(() => {
-      logger.info('service worker registration successful')
-      initPrefetching()
+    .then(registration => {
+      // If the service worker is active, we can start prefetching immediately.
+      if (registration.active) {
+        logger.info('service worker activated')
+        initPrefetching()
+        return
+      }
+
+      // Otherwise, we need to wait until the service worker is activated.
+      registration.addEventListener('updatefound', () => {
+        const installingWorker = registration.installing
+        installingWorker.onstatechange = e => {
+          if (e.target.state === 'activated') {
+            installingWorker.onstatechange = null
+            logger.info('service worker registered and activated')
+            initPrefetching()
+          }
+        }
+      })
     })
     .catch(err =>
       logger.warn('service worker registration failed', err.message)
